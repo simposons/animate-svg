9d37dab170ca48be267869c47a1f17c1c469518e
lupeng <295840752@qq.com>
2023-03-09 16:51:24 +0800

    index提交

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var s = '彼时彼刻，恰如此时此刻';
+console.log(s);
+var a = '1';
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const s: string = '彼时彼刻，恰如此时此刻'
+console.log(s)
+const a = '1'
+console.log(a)
