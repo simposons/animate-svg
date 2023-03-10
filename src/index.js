8279c234b36a4927962f31b335cd5feca7407ce8
simposons <295840752@qq.com>
2023-03-10 17:28:03 +0800

    主功能开发

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-var s = '彼时彼刻，恰如此时此刻';
-console.log(s);
-var a = '1';
-console.log(a);
-//# sourceMappingURL=index.js.map
+'use strict';
+class AnimateSvg {
+    constructor(key,options={}) {
+        this.key=key
+        this.svgDocument=document.querySelector(this.key)
+        this.options={
+            file:'',
+            animation:'',
+            duration: 200,
+            ...options
+        }
+        this.init(this.svgDocument,this.options)
+    }
+    init(_document,_options){
+        if(!_document){
+            console.error('')
+            return
+        }
+        if(!_options){
+            console.error('')
+            return
+        }
+
+    }
+}
+
+export default AnimateSvg
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
-const s: string = '彼时彼刻，恰如此时此刻'
-console.log(s)
-const a = '1'
-console.log(a)
+interface Option {
+    readonly file: string;
+    readonly animation: string;
+    readonly duration: string|number;
+}
+export default class AnimateSvg {
+    protected  key:string=''
+    protected  options:Option={
+        file:'',
+        animation:'',
+        duration: 200,
+    }
+    protected svgDocument:HTMLDivElement|null
+    public constructor(key:string,options:object) {
+        this.key = key
+        this.options={
+            ...this.options,
+            ...options
+        }
+        if(!this.key){
+            console.error('无法获取key,请传入')
+            return
+        }
+        this.init()
+    }
+    protected init():void{
+        this.svgDocument=document.querySelector(this.key)
+        
+
+    }
+}
